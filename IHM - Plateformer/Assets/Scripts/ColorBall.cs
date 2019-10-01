9b3a288abf28924e1605222c375d96dc217ef3b4
Immoskyl <rromain27@neuf.fr>
2019-10-01 16:14:11 +0100

    basic inputs are managed for both the controller and the keyboard

---
+++ IHM - Plateformer/Assets/Scripts/ColorBall.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorBall : MonoBehaviour
+{
+    [SerializeField]
+    private float speedCoef = 0.3f;
+
+    public float SpeedCoef
+    {
+        get => speedCoef;
+        set => speedCoef = value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //////////////////////////////////////////////////////
+    ///                OBSERVER
+    //////////////////////////////////////////////////////
+    
+    
+    private void OnEnable()
+    {
+        MyInputManager.OnButtonAIsPushed += ButtonAIsPressed;
+        MyInputManager.OnButtonBIsPushed += ButtonBIsPressed;
+        MyInputManager.OnButtonXIsPushed += ButtonXIsPressed;
+        MyInputManager.OnButtonYIsPushed += ButtonYIsPressed;
+        MyInputManager.OnNoButtonIsPushed += NoButtonIsPressed;
+        MyInputManager.OnMovementIsTriggered += MovementIsTriggered;
+    }
+
+    private void OnDisable()
+    {
+        MyInputManager.OnButtonAIsPushed -= ButtonAIsPressed;
+        MyInputManager.OnButtonBIsPushed -= ButtonBIsPressed;
+        MyInputManager.OnButtonXIsPushed -= ButtonXIsPressed;
+        MyInputManager.OnButtonYIsPushed -= ButtonYIsPressed;
+        MyInputManager.OnNoButtonIsPushed -= NoButtonIsPressed;
+        MyInputManager.OnMovementIsTriggered -= MovementIsTriggered;
+    }
+    
+        
+    //////////////////////////////////////////////////////
+    ///                INPUTS
+    //////////////////////////////////////////////////////
+
+
+    
+    void ButtonAIsPressed()
+    {
+        GetComponent<SpriteRenderer>().color = Color.yellow;
+    }
+
+    void ButtonBIsPressed()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    void ButtonXIsPressed()
+    {
+        GetComponent<SpriteRenderer>().color = Color.magenta;
+    }
+
+    void ButtonYIsPressed()
+    {
+        GetComponent<SpriteRenderer>().color = Color.black;
+    }
+
+    void NoButtonIsPressed()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    void MovementIsTriggered(float horizontalAxis, float verticalAxis)
+    {
+        transform.Translate(horizontalAxis * speedCoef, verticalAxis * speedCoef, 0);
+    }
+
+}
