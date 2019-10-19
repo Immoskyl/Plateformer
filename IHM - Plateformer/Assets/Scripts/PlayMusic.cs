c1f35870361b81031f9e8ad1d46b2a518aa33be3
Immoskyl <rromain27@neuf.fr>
2019-10-19 13:45:21 +0100

    music added

---
+++ IHM - Plateformer/Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
+        audioSource.volume = 0.4f;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
