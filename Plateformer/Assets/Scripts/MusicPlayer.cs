84ab6d6da13bd0a1f322f2cc001930f5e852b350
Immoskyl <rromain27@neuf.fr>
2019-10-22 15:47:08 +0100

    better jump feeling

---
+++ Plateformer/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MusicPlayer : MonoBehaviour
+{
+    
+    [SerializeField]
+    public AudioSource music1;
+    
+    private AudioSource activeMusic;
+
+    public AudioSource ActiveMusic
+    {
+        get => activeMusic;
+        set => activeMusic = value;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeMusic = music1;
+    }
+
+    public void ChangeMusic(AudioSource a)
+    {
+        activeMusic.Stop ();
+        activeMusic = a;
+        activeMusic.loop = true;
+        activeMusic.volume = 0.2f;
+        activeMusic.Play();
+    }
+}
