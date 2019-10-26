00232c161855861eed61849c0a4575abb417d308
Immoskyl <rromain27@neuf.fr>
2019-10-26 20:33:40 +0100

    tentative de creer une scene de menu mais ca bug: 1)les boutons ne sont soudainement plus clickables =( et 2)ce serait top que le sprite puisse passer devant les bouttons et qu'on puisse envoyer un signal button.clicked() lorsque le sprite est dessus && qu'on appuie sur saut (tout ca c'est pour l'intégration manette)

---
+++ Plateformer/Assets/Scripts/Menu/ChooseLevels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChooseLevels : MonoBehaviour
+{
+
+    /**
+     * Load a Scene by its index referenced in the Build Manager
+     */
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    /**
+     * Ensure to quit the game, whether it is running embed in Unity Launcher or a build version
+     */
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Plateformer/Assets/Scripts/Menu/CursorManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(1f, 20f)]
+    private float speedCoef;
+
+    public float SpeedCoef
+    {
+        get => speedCoef;
+        set => speedCoef = value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Clic();
+        }
+
+        transform.Translate(Input.GetAxis("Horizontal") * SpeedCoef, Input.GetAxis("Vertical") * SpeedCoef, 0);
+    }
+
+    /**
+     *  @todo
+     */
+    void Clic()
+    {
+        /*
+        if (transform.position = true)
+        {
+        }
+        */
+    }
+}
